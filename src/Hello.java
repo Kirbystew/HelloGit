97bee9110a0207b7e3d3ed886338b4dca77a76bb
Gavin <Gavin@GavinsPC.unl.edu>
2023-08-25 10:04:13 -0500

    Made it so it prints out my name

---
+++ src/Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Author: Gavin Lane (glane6@huskers.unl.edu) 
+ * Date: 08/22/2023
+ * 
+ * A simple "Hello World!" program in Java
+ * 
+ * @author glane6
+ *
+ */
+public class Hello {
+
+	public static void main(String args[]) {
+		System.out.println("Gavin Lane!");
+		
+		}
+}
